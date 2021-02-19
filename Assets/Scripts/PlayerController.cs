ec1e69282cb17c5a992fdafa52a3c7b2bb09ba65
PizzaMetAnanasIsLekker <330864@student.mboutrecht.nl>
2021-02-19 09:28:19 +0100

    a

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool gameOver = false;
+    public ParticleSystem explosion;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private Rigidbody rb;
+    private const float jumpForce = 30;
+    private const float gravityModifier = 10;
+    private bool onGround = true;
+    private Quaternion startRotation;
+    private Animator anim;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        Physics.gravity *= gravityModifier;
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true && gameOver == false)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            onGround = false;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+        }
+        if (onGround == false)
+        {
+            transform.Rotate(0.0f, 2000.0f * Time.deltaTime, 0.0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+            if (gameOver == false)
+            {
+                dirtParticle.Play();
+            }
+            transform.rotation = startRotation;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            anim.SetBool("Death_b", true);
+            explosion.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
+            Debug.Log("GAME OVER!");
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+
+    private Vector3 spawnPos = new Vector3(25.0f, 0.0f, -1.285f);
+    private const float startDelay = 2.0f;
+    private const float repeatDelay = 2.0f;
+    private PlayerController playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
